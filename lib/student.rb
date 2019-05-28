bf0f0d15818c21de1003972b32440f0760d6ab94
Sandesh Karki <karkisandesh@hotmail.com>
2019-05-28 04:11:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,6 @@ class Student
   def add_student_attributes(attributes_hash)
       attributes_hash.each do |key, value|
       self.send(("#{key}="), value)
-      # @@all << self
     end
   end
 
