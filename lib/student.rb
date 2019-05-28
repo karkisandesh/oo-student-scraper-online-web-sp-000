597942f550234a7fcfd45774376dc8d4ed831032
Sandesh Karki <karkisandesh@hotmail.com>
2019-05-28 04:00:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-   students_array.each{|student| Student.new(student)}
-   binding.pry
+   students_array.each{|student| 
+   binding.pry Student.new(student)}
+   
   end
 
   def add_student_attributes(attributes_hash)
