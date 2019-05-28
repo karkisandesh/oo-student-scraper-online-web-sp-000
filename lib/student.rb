78cb7cadb5124f8e48e151cac4d7f51ccdebdb8e
Sandesh Karki <karkisandesh@hotmail.com>
2019-05-28 03:56:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,13 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
     student_hash.each do |key, value|
       self.send(("#{key}="), value)
       @@all << self
-   
-  end
-    
+    end
   end
 
   def self.create_from_collection(students_array)
