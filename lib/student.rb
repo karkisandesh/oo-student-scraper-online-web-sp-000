5c11b6b962e0ad68b24f8d1f3a5a5d3770a45c25
Sandesh Karki <karkisandesh@hotmail.com>
2019-05-28 03:59:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-  students_array.each_with_index do |index, key|
-      self.send(("#{key}="), value)
-      @@all << self
-    end.uniq
+   students_array.each{|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
